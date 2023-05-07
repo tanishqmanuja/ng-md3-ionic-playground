c5450e5c6e6b1ce3a43eeea712a256745c13f853
tanishqmanuja <tanishqmanuja@gmail.com>
2023-05-07 17:17:29 +0530

    feat: app config with ionic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { PreloadAllModules, RouteReuseStrategy, provideRouter, withPreloading } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    importProvidersFrom(IonicModule.forRoot())
+  ]
 };
