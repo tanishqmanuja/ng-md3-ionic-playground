70d53e986d34993ce8b217ea59cd89e67dc1b328
tanishqmanuja <tanishqmanuja@gmail.com>
2023-05-07 17:06:24 +0530

    perf: cchedule application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+/* Schedule Application Bootstrap to reduce Total Blocking Time (TBT)*/
+setTimeout(()=>{
+  bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+})
+
